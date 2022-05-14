bad6a7638a4f214c148561ddeac650215434d931
Michal Binder <michalabr5@gmail.com>
2022-05-14 13:30:05 +0300

    DB Connection

--- lostPet/urls.py
+++ lostPet/urls.py
@@ -3,5 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('home/', include('website.urls')),
+    path('post/', include('post.urls')),
     path('admin/', admin.site.urls),
 ]
