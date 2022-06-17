a73f675e6ae95e868d3d5886ca5ded6af0b0ef5d
Oshrey Avraham <oshrey16@gmail.com>
2022-06-17 14:02:41 +0300

    clean unnecessary files

--- lostPet/lostPet/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path('home/', include('website.urls')),
-    path('post/', include('post.urls')),
-    path('admin/', admin.site.urls),
-    path('/captcha',include("captcha.urls"))
-]
