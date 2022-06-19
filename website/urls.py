40043ebb75396a796a2a7abe126c8f434306e51e
Oshrey Avraham <oshrey16@gmail.com>
2022-06-20 01:50:58 +0300

    fix base html and create aboutUs page

--- website/urls.py
+++ website/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.allp, name='all'),
+    path('aboutUs', views.aboutUs, name='aboutUs'),
     path('delete/<int:id>', views.delete, name='delete'),
     path('com/<int:id>', views.com, name='com'),
 ]
