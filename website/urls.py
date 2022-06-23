92eee1d24d8cdc50c055e6e519224bdc4d88277a
Oshrey Avraham <oshrey16@gmail.com>
2022-06-23 18:12:49 +0300

    create error page sms

--- website/urls.py
+++ website/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.allp, name='all'),
     path('aboutUs', views.aboutUs, name='aboutUs'),
+    path('errorsms', views.errorsms, name='errorsms'),
     path('delete/<int:id>', views.delete, name='delete'),
     path('com/<int:id>', views.com, name='com'),
 ]
