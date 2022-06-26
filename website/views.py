c210c0dc4817f27bbf42b772df884928cda331d1
Oshrey Avraham <oshrey16@gmail.com>
2022-06-26 15:53:46 +0300

    sms template

--- website/views.py
+++ website/views.py
@@ -25,15 +25,28 @@ def delete(request, id):
 
 SUBJECT_STR = u"למישהו יש חדש בנושא הדיווח!!"
 
-def bodyCreate(freetext,firstName,lastName,phone,email,hosturl,id):
+
+def bodyCreate(freetext, firstName, lastName, phone, email, hosturl, id):
     text = ""
     text += freetext
     text += "\n\n==================================\n"
-    text += u"   פרטים ליצירת קשר: "+firstName + " "+lastName+" "+phone+" "+email
-    text += "\n\n====================\n"+u"למחיקת הפרסום במידה והדיווח טופל:          "
+    text += u"   פרטים ליצירת קשר: {} {} {} {}".format(
+        firstName, lastName, phone, email)
+    text += "\n\n====================\n" + \
+        u"למחיקת הפרסום במידה והדיווח טופל:          "
     text += (hosturl+"delete/"+str(id))
     print(text)
     return text
+
+
+def bodyCreateSMS(freetext, firstName, lastName, phone, email):
+    text = ""
+    text += u"למישהו יש חדש בנושא הדיווח!!\n"
+    text += freetext
+    text += "\n\n==================================\n"
+    text += u"פרטים ליצירת קשר: {} {} {} {} ".format(
+        firstName, lastName, phone, email)
+
 
 def com(request, id):
     if request.method == 'GET':
@@ -52,23 +65,23 @@ def com(request, id):
                 new_mail = form.save()
                 msgToHost = EmailMessage(
                     SUBJECT_STR,
-                    bodyCreate(freetext,firstName,lastName,phone,email,request.build_absolute_uri("/home/"),id),
+                    bodyCreate(freetext, firstName, lastName, phone,
+                               email, request.build_absolute_uri("/home/"), id),
                     EMAIL_HOST_USER,
                     [pet.pub_email],
                 )
-                # TODO UPDATE EMAIL
+                # TODO-  UPDATE EMAIL
                 # msgToHost.send()
 
             except BadHeaderError:
                 return HttpResponse('Invalid header found.')
+
             to = "+972"+pet.pub_phone
             client = Client(settings.TWILIO_ACCOUNT_SID,
                             settings.TWILIO_AUTH_TOKEN)
             try:
                 response = client.messages.create(
-                    body=(u"למישהו יש חדש בנושא הדיווח!!\n" +
-                          freetext+"\n\n==================================\n"+u"   פרטים ליצירת קשר: "+firstName + " "+lastName+" "+phone+" "+email),
-                    to=to, from_=settings.TWILIO_PHONE_NUMBER)
+                    body=bodyCreateSMS(freetext, firstName, lastName, phone, email), to=to, from_=settings.TWILIO_PHONE_NUMBER)
                 return redirect('/home')
             except:
                 return redirect('/home/errorsms')
